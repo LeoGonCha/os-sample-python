932144d1909737bf894d45e2a8b78d07fd3b9cb7
LeoGonCha <leogoncha@gmail.com>
2018-07-31 13:15:20 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -13,25 +13,26 @@ def JsonHandler():
         content = request.get_json()
         # Handle project changes
         if content['event_name'] == "project_create":
-            pipe = store.pipeline()
-            pipe.hmset(content['name'], {"group": content['owner_name']})
-            pipe.hmset(content['name'], {"branches": map_branches })
-            pipe.execute()
+            #pipe = store.pipeline()
+            #pipe.hmset(content['name'], {"group": content['owner_name']})
+            #pipe.hmset(content['name'], {"branches": map_branches })
+            #pipe.execute()
             logger.info('Project {0} Created.'.format(content['name']))
             return "OK"
         elif content['event_name'] == "push":
-            update_consul_last_push(content)
+            #update_consul_last_push(content)
+            logger.info('Project {0} push.'.format(content['name']))
             return "OK"
         elif content['event_name'] == "project_destroy":
-            store.delete(content['name'])
+            #store.delete(content['name'])
             logger.info('Project {0} destoried.'.format(content['name']))
             return "OK"
         elif content['event_name'] == "project_rename":
-            store.rename(content['old_path_with_namespace'].split('/')[1], content['name'])
+            #store.rename(content['old_path_with_namespace'].split('/')[1], content['name'])
             logger.info('Project renamed from {0} to {1}'.format(content['old_path_with_namespace'].split('/')[1], content['name']))
             return "OK"
         elif content['event_name'] == "project_transfer":
-            store.hmset(content['name'], {"group": content['path_with_namespace'].split('/')[0]})
+            #store.hmset(content['name'], {"group": content['path_with_namespace'].split('/')[0]})
             logger.info('Project {0} transferd'.format(content['name']))
             return "OK"
     else:
